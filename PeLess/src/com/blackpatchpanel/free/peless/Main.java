b1c6327c06ad78392f8fc3dd38b89886b41630dd
Paul Elliott <pelliott@blackpatchpanel.com>
2016-11-18 01:17:56 -0500

    Open with fxml file!

--- PeLess/src/com/blackpatchpanel/free/peless/Main.java
+++ PeLess/src/com/blackpatchpanel/free/peless/Main.java
@@ -1,7 +1,9 @@
 package com.blackpatchpanel.free.peless;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("/peless.css").toExternalForm());
 			primaryStage.setScene(scene);
