13969cd7ff1638f1b77736b08e040a5bc7b8d1f3
Paul Elliott <pelliott@blackpatchpanel.com>
2016-11-20 00:16:54 -0500

    add Base controller

---
+++ PeLess/src/com/blackpatchpanel/free/peless/controller/Base.java
@@ -0,0 +1,13 @@
+package com.blackpatchpanel.free.peless.controller;
+
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXML;
+
+public class Base {
+    public Base() {
+		super();
+	}
+
+	@FXML
+    private VBox container;
+}
