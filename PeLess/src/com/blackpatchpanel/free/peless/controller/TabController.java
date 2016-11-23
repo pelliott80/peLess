495f946b5bcc035faa9c1b701bb068aa258df5b8
Paul Elliott <pelliott@blackpatchpanel.com>
2016-11-23 09:43:45 -0500

    fix controller has no such field messages rename tab.fxml file make text area non editable

---
+++ PeLess/src/com/blackpatchpanel/free/peless/controller/TabController.java
@@ -0,0 +1,19 @@
+package com.blackpatchpanel.free.peless.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+
+public class TabController {
+	
+	@FXML
+	private Tab tabControler = new Tab();
+	
+	@FXML
+	private TextArea textArea = new TextArea();
+
+	public TabController() {
+		super();
+	}
+
+}
