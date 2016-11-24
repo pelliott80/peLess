faf9fe5c3bb283c9207594b9ccd8c91b3eb298e9
Paul Elliott <pelliott@blackpatchpanel.com>
2016-11-24 02:02:18 -0500

    created getFileContentList to turn a file into a list of strings.

--- PeLess/src/com/blackpatchpanel/free/peless/model/Model.java
+++ PeLess/src/com/blackpatchpanel/free/peless/model/Model.java
@@ -1,9 +1,21 @@
 package com.blackpatchpanel.free.peless.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 public class Model {
 
 	public Model() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public List<String>  getFileContentList(File file) throws IOException {
+
+		// turn file into a path with File.toPath
+		// use path to return a list of lines. from the file.
+		return Files.readAllLines(file.toPath());
 	}
 
 }
